56892bfc40ee6d27b208b7882fac2e975575813a
Plouis53 <Plouis53@gmail.com>
2023-04-24 16:27:38 -0400

    fix images

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,24 +1,27 @@
 import React from "react";
 import "./Header.css";
 
-const Header = () => {
+const Header = ({ onCreateModal }) => {
   console.log("Header");
 
   return (
     <header className="header">
       <div className="header__logo">
         <div>
-          <img src="logo.svg" alt="logo" />
+          <img src={require("../Images/logo.svg").default} alt="logo" />
         </div>
         <div>Date</div>
       </div>
       <div className="header__avatar-logo">
         <div>
-          <button type="text"> Add New Clothes </button>
+          <button type="text" onClick={onCreateModal}>
+            {" "}
+            Add New Clothes{" "}
+          </button>
         </div>
         <div> Name</div>
         <div>
-          <img src="Images/avatar.svg" alt="avatar" />
+          <img src={require("../Images/avatar.svg").default} alt="avatar" />
         </div>
       </div>
     </header>
