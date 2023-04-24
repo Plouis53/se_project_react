b8acb42a029e1c9477a219295b5dd9c8e2ad13bd
Plouis53 <Plouis53@gmail.com>
2023-04-23 21:34:54 -0400

    Add features to App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="header">
+        <div>Logo</div>
+        <div>Date</div>
+        <div>
+          <button type="text"> Add New Clothes </button>
+        </div>
+        <div> Name</div>
+        <div>Avatar Logo</div>
       </header>
     </div>
   );
