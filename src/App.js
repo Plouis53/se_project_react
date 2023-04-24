7136fdc1211635c41868ebd7ebae1e43b9b89937
Plouis53 <Plouis53@gmail.com>
2023-04-24 12:33:37 -0400

    created day/night folders

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           <div>75F</div>
           <div>
             {" "}
-            <img />{" "}
+            <img src="./Images/day/sunny" />
           </div>
         </section>
 
