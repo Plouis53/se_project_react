2adc7cf9891a9b5bb4999dffb365b21790770ae8
Plouis53 <Plouis53@gmail.com>
2023-04-25 15:05:10 -0400

    imported react

--- src/ItemModal/ItemModal.js
+++ src/ItemModal/ItemModal.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const ItemModal = ({ selectedCard }) => {
   console.log("item modal");
   return (
--- src/ModalWithForm/ModalWithForm.js
+++ src/ModalWithForm/ModalWithForm.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./ModalWithForm.css";
 
 const ModalWithForm = ({
