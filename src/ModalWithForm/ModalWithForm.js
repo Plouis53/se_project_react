1b26977ab83b99fe198b4657a54e85f19972acc1
Plouis53 <Plouis53@gmail.com>
2023-04-26 13:47:38 -0400

    New updates

--- src/ModalWithForm/ModalWithForm.js
+++ src/ModalWithForm/ModalWithForm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./ModalWithForm.css";
+import Close Icon 
 
 const ModalWithForm = ({
   children,
@@ -11,10 +12,12 @@ const ModalWithForm = ({
   return (
     <div className={`modal modal_type_${name}`}>
       <div className="modal_content">
-        <button type="button" onClick={onClose}>
+
+        <button type="button" className="popup__button"  aria-label="Close" onClick={onClose}>
           Close
+          <img className="popup__close" alt="Close Button Image" src=""
         </button>
-        <h3>{title}</h3>
+        <h3 className="popup__header">New Garment</h3>
         <form>{children}</form>
         <button type="submit">{buttonText}</button>
       </div>
