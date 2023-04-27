939e2c8841192ef387f2765945cc2cda76c4f89c
Plouis53 <Plouis53@gmail.com>
2023-04-26 23:46:03 -0400

    correct modal

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import ModalWithForm from "./ModalWithForm/ModalWithForm";
 import ItemModal from "./ItemModal/ItemModal";
 import { getForecastWeather, parseWeatherData } from "./utils/weatherApi";
 import React, { useEffect, useState } from "react";
-import closeIcon from "../src/Images/Union.svg";
 
 function App() {
   const weatherTemp = "75°F";
@@ -42,95 +41,81 @@ function App() {
       <Footer />
       {activeModal === "create" && (
         <ModalWithForm title="New Garment" onClose={handleCloseModal}>
-          <div className="modal__container-form">
-            <form className="modal__form" name="add">
-              <fieldset className="modal__fieldset">
-                <button
-                  type="button"
-                  className="modal__button"
-                  aria-label="Close"
-                >
-                  <img
-                    className="modal__close"
-                    alt="Close button image"
-                    src={closeIcon}
-                  ></img>
-                </button>
-                <h2 className="modal__header">New Garment</h2>
-                <label className="modal__label">
-                  Name
-                  <input
-                    className="modal__input"
-                    type="text"
-                    placeholder="Name"
-                    required
-                    name="name"
-                    id="input-name"
-                    miniLength="1"
-                    maxLength="30"
-                  />
-                </label>
-                <label className="modal__label">
-                  Image
-                  <input
-                    className="modal__input"
-                    type="url"
-                    placeholder="Image Link"
-                    required
-                    name="Image link"
-                    id="input-link"
-                    miniLength="1"
-                    maxLength="30"
-                  />
-                </label>
-                <p className="modal__text">Select the weather type:</p>
-                <div className="modal__input-container">
-                  <div>
-                    <input
-                      className="modal__input-button"
-                      type="radio"
-                      id="hot"
-                      value="hot"
-                      name="rangeOfTemp"
-                    />
-                    <label className="modal__temp-ranges">Hot</label>
-                  </div>
-                  <div>
-                    <input
-                      className="modal__input-button"
-                      type="radio"
-                      id="warm"
-                      value="warm"
-                      name="rangeOfTemp"
-                    />
-                    <label className="modal__temp-ranges">Warm</label>
-                  </div>
-                  <div>
-                    <input
-                      className="modal__input-button"
-                      type="radio"
-                      id="cold"
-                      value="cold"
-                      name="rangeOfTemp"
-                    />
-                    <label className="modal__temp-ranges">Cold</label>
-                  </div>
-                </div>
-                <button
-                  className="modal__save"
-                  type="submit"
-                  aria-label="Save"
-                  id="add-save"
-                >
-                  Add garment
-                </button>
-              </fieldset>
-            </form>
-          </div>
+          <fieldset className="modal__fieldset">
+            <label className="modal__label">
+              Name
+              <input
+                className="modal__input"
+                type="text"
+                placeholder="Name"
+                required
+                name="name"
+                id="input-name"
+                miniLength="1"
+                maxLength="30"
+              />
+            </label>
+            <label className="modal__label">
+              Image
+              <input
+                className="modal__input"
+                type="url"
+                placeholder="Image Link"
+                required
+                name="Image link"
+                id="input-link"
+              />
+            </label>
+            <p className="modal__text">Select the weather type:</p>
+            <div className="modal__input-container">
+              <div>
+                <input
+                  className="modal__input-button"
+                  type="radio"
+                  id="hot"
+                  value="hot"
+                  name="rangeOfTemp"
+                />
+                <label className="modal__temp-ranges">Hot</label>
+              </div>
+              <div>
+                <input
+                  className="modal__input-button"
+                  type="radio"
+                  id="warm"
+                  value="warm"
+                  name="rangeOfTemp"
+                />
+                <label className="modal__temp-ranges">Warm</label>
+              </div>
+              <div>
+                <input
+                  className="modal__input-button"
+                  type="radio"
+                  id="cold"
+                  value="cold"
+                  name="rangeOfTemp"
+                />
+                <label className="modal__temp-ranges">Cold</label>
+              </div>
+            </div>
+            {/* <button
+            className="modal__save"
+            type="submit"
+            aria-label="Save"
+            id="add-save"
+          >
+            Add garment
+          </button> */}
+          </fieldset>
         </ModalWithForm>
       )}
       {activeModal === "preview" && (
-        <ItemModal selectedCard={selectedCard} onClose={handleCloseModal} />
+        <ItemModal
+          selectedCard={selectedCard}
+          onClose={handleCloseModal}
+          closeIcon={undefined}
+        />
       )}
     </div>
   );
--- src/ModalWithForm/ModalWithForm.js
+++ src/ModalWithForm/ModalWithForm.js
@@ -1,25 +1,49 @@
 import React from "react";
 import "./ModalWithForm.css";
-// import closeIcon from "./Images/Union:close.svg";
 
-const ModalWithForm = ({
-  children,
-  buttonText = "Add garment",
-  onClose,
-  name,
-}) => {
+// import closeIcon from "../Images/Union.svg";
+
+const ModalWithForm = ({ children, onClose, name }) => {
   return (
     <div className={`modal modal_type_${name}`}>
-      <div className="modal_container-form">
+      <div className="modal__container-form">
+        <form className="modal__form" name="add">
+          <fieldset className="modal__fieldset">
+            <button
+              type="button"
+              className="modal__close"
+              aria-label="Close"
+              onClick={onClose}
+            >
+              {/* <img
+                className="modal__close"
+                alt="Close button image"
+                src={closeIcon}
+              ></img> */}
+            </button>
+            <h2 className="modal__header">New Garment</h2>
+            {/* <div className="modal_container-form">
         <button
           type="button"
           className="modal__close"
           aria-label="Close"
           onClick={onClose}
         ></button>
-        <h2 className="modal__header">New Garment</h2>
-        <form>{children}</form>
-        <button type="submit">{buttonText}</button>
+        <h2 className="modal__header">New Garment</h2> */}
+            <form>{children}</form>
+            <button className="modal__footer" type="submit" id="modal-add">
+              Add garment
+            </button>
+            {/* <button
+              className="modal__save"
+              type="submit"
+              aria-label="Save"
+              id="add-save"
+            >
+              Add garment
+            </button> */}
+          </fieldset>
+        </form>
       </div>
     </div>
   );
