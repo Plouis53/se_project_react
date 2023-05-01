f4a9604d1a3f1b09803168c29dc01885b4f0ec53
Plouis53 <Plouis53@gmail.com>
2023-05-01 12:55:27 -0400

    changed location of card name

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,20 +2,18 @@ import React from "react";
 import "../blocks/Card.css";
 
 const Card = ({ item, onSelectCard }) => {
-  console.log(Card.name)
+  console.log(Card.name);
   return (
     <div>
       <div className="card">
         <div className="card__container">
+          <p className="card__name"> {item.name} </p>
           <img
             src={item.link}
             className="card__image"
             onClick={() => onSelectCard(item)}
           />
-          <p className="card__name"> {item.name} </p>
         </div>
-        {/* <div className="card__container"> */}
-        {/* <p className="card__name"> {item.name} </p> */}
       </div>
     </div>
   );
