92ab02539355504498feb5793119e11136b34666
Plouis53 <Plouis53@gmail.com>
2023-05-02 16:25:42 -0400

    added alt=item.name

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,16 +5,15 @@ import "../blocks/Page.css";
 const Card = ({ item, name, onSelectCard, weather, _id, id, link }) => {
   console.log(Card.name);
   return (
-    <div>
-      <div className="card">
-        <div className="card__container">
-          <p className="card__name"> {item.name} </p>
-          <img
-            src={item.link}
-            className="card__image"
-            onClick={() => onSelectCard(item, name, weather, _id, id, link)}
-          />
-        </div>
+    <div className="card">
+      <div className="card__container">
+        <p className="card__name"> {item.name} </p>
+        <img
+          src={item.link}
+          className="card__image"
+          onClick={() => onSelectCard(item, name, weather, _id, id, link)}
+          alt={item.name}
+        />
       </div>
     </div>
   );
