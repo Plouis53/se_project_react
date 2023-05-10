d21e2e7c613bd4296a89bb55db1ffb41e7c1500f
Plouis53 <Plouis53@gmail.com>
2023-05-10 16:39:36 -0400

    install confirmation

--- src/components/ItemModal.js
+++ src/components/ItemModal.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "../blocks/ItemModal.css";
 import "../components/ModalWithForm";
 
 const ItemModal = ({ selectedCard, onClose, onDelete }) => {
+  const [showConfirmationModal, setShowConfirmationModal] = useState(false);
+
   const handleDelete = () => {
     onDelete(selectedCard.id);
+    setShowConfirmationModal(false);
+  };
+
+  const handleOpenConfirmationModal = () => {
+    setShowConfirmationModal(true);
+  };
+
+  const handleCloseConfirmationModal = () => {
+    setShowConfirmationModal(false);
   };
 
   return (
@@ -29,9 +40,36 @@ const ItemModal = ({ selectedCard, onClose, onDelete }) => {
               Weather Type: {selectedCard.weather}
             </p>
           </div>
-          <button className="popup__delete" onClick={handleDelete}>
+          <button
+            className="popup__delete"
+            onClick={handleOpenConfirmationModal}
+          >
             Delete
           </button>
+          {showConfirmationModal && (
+            <div className="popup__confirmation">
+              <p>Are you sure you want to delete this item?</p>
+              <p className="popup__text_confirm">This action is irreversible</p>
+              <div className="popup__confirmation-buttons">
+                <button
+                  className="popup__button_confirm"
+                  type="button"
+                  aria-label="Confirm"
+                  onClick={handleDelete}
+                >
+                  Yes, delete item
+                </button>
+                <button
+                  className="popup__button_cancel"
+                  type="button"
+                  aria-label="Cancel"
+                  onClick={handleCloseConfirmationModal}
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
@@ -39,49 +77,3 @@ const ItemModal = ({ selectedCard, onClose, onDelete }) => {
 };
 
 export default ItemModal;
-
-
-
-
-// import React from "react";
-// import "../blocks/ItemModal.css";
-// import "../components/ModalWithForm";
-
-
-// const handleDelete = () => {
-//   onDelete(selectedCard.id);
-// };
-
-// const ItemModal = ({ selectedCard, onClose, onDelete }) => {
-//   return (
-//     <div className={`popup__container-image`}>
-//       <div className="popup__photo">
-//         <img
-//           className="popup__image"
-//           src={selectedCard.link}
-//           alt={selectedCard.name}
-//         />
-//         <button
-//           className="popup__close"
-//           type="button"
-//           aria-label="Close"
-//           id="popup-button"
-//           onClick={onClose}
-//         ></button>
-//         <div className="popup__subcontainer">
-//           <div>
-//             <h2 className="popup__title">{selectedCard.name}</h2>
-//             <p className="popup__weather">
-//               Weather Type: {selectedCard.weather}
-//             </p>
-//           </div>
-//           <button className="popup__delete" onClick={onDelete}>
-//             Delete
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ItemModal;
