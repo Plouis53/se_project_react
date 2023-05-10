fe20383de5effab5e797b22614e5e2fb108d8102
Plouis53 <Plouis53@gmail.com>
2023-05-10 11:36:47 -0400

    dev switch toggle

--- src/components/TempSwitch.js
+++ src/components/TempSwitch.js
@@ -12,36 +12,39 @@ const ToggleSwitch = () => {
 
   return (
     <div className="switch">
-      <div className="switch__container">
-        <input
-          className="switch__input"
-          type="checkbox"
-          name="switch-checkbox"
-          value={currentTempUnit}
-          id="switch"
-          onChange={handleToggleSwitch}
-          checked={isChecked}
-        ></input>
-        <span
-          className={
-            currentTempUnit === "F" ? "switch__F" : "switch__C"
-          }
-        ></span>
-        <p
-          className={`switch__temp-F ${
-            currentTempUnit === "F" ? "switch__active" : ""
-          }`}
-        >
-          F
-        </p>
-        <p
-          className={`switch__temp-C ${
-            currentTempUnit === "C" ? "switch__active" : ""
-          }`}
-        >
-          C
-        </p>
-      </div>
+      {/* <div className="switch__container"> */}
+      <input
+        className="switch__input"
+        type="checkbox"
+        name="switch-checkbox"
+        value={currentTempUnit}
+        id="switch"
+        onChange={handleToggleSwitch}
+        checked={isChecked}
+      ></input>
+      <label className="switch__label">
+        <span className="switch__button">
+          <div className="switch__container">
+            <span
+              className={currentTempUnit === "F" ? "switch__F" : "switch__C"}
+            ></span>
+            <p
+              className={`switch__temp-F ${
+                currentTempUnit === "F" ? "switch__active" : ""
+              }`}
+            >
+              F
+            </p>
+            <p
+              className={`switch__temp-C ${
+                currentTempUnit === "C" ? "switch__active" : ""
+              }`}
+            >
+              C
+            </p>
+          </div>
+        </span>
+      </label>
     </div>
   );
 };
