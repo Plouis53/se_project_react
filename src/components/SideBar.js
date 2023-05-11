a959b7b6f8d32d287198d76f8f0b8962defb63f3
Plouis53 <Plouis53@gmail.com>
2023-05-11 15:51:46 -0400

    updates

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -5,8 +5,10 @@ import "../blocks/SideBar.css";
 const SideBar = () => {
   return (
     <div className="sidebar">
-      <img src={headerAvatar} className="sidebar__avatar" alt="avatar" />
-      <div className="sidebar__name">Phillippe Louis</div>
+      <div className="sidebar__container">
+        <img src={headerAvatar} className="sidebar__avatar" alt="avatar" />
+        <div className="sidebar__name">Phillippe Louis</div>
+      </div>
     </div>
   );
 };
