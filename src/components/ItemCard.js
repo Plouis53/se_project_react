f72e6ff9faeeea276a9e0dec65a9dc4bb12be3d7
Plouis53 <Plouis53@gmail.com>
2023-05-11 15:22:30 -0400

    remove markers

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,20 +1,23 @@
 import React from "react";
 import "../blocks/Card.css";
 
+const ItemCard = ({ item, onSelectCard }) => {
+  const handleCardClick = () => {
+    onSelectCard(item);
+  };
 
-const ItemCard = ({ item, name, onSelectCard, weather, _id, id, link }) => {
   return (
-    <li className="card">
+    <div className="card">
       <div className="card__container">
         <p className="card__name">{item.name}</p>
         <img
           src={item.link}
           className="card__image"
-          onClick={() => onSelectCard(item, name, weather, _id, id, link)}
+          onClick={handleCardClick}
           alt={item.name}
         />
       </div>
-    </li>
+    </div>
   );
 };
 
