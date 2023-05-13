32fb107805f10d664ed608657ad282e8112d27d9
Plouis53 <Plouis53@gmail.com>
2023-05-12 21:04:32 -0400

    captilize deleteitem

--- src/components/Api.js
+++ src/components/Api.js
@@ -28,7 +28,7 @@ export const addItem = async (newItem) => {
 };
 
 // Delete a clothing item by ID
-export const deleteItem = async (id) => {
+export const DeleteItem = async (id) => {
   const response = await fetch(`${baseUrl}/items/${id}`, {
     method: "DELETE",
   });
