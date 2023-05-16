2fbacaf0bed195fc701699c5c50918e0ff7a0683
Plouis53 <Plouis53@gmail.com>
2023-05-16 13:53:12 -0400

    created sets of functions

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,14 +1,19 @@
-export function getItems() {
+function _checkResponse(res) {
+  if (res.ok) {
+    return res.json();
+  }
+  return Promise.reject(`Error${res.status}`);
+}
+
+function getItems() {
   const baseUrl =
     "https://my-json-server.typicode.com/Plouis53/se_project_react";
-
   return fetch(`${baseUrl}/items`).then(_checkResponse);
 }
 
-export function addItem(name, imageUrl, weather) {
+function addItem(name, imageUrl, weather) {
   const baseUrl =
     "https://my-json-server.typicode.com/Plouis53/se_project_react";
-
   return fetch(`${baseUrl}/items`, {
     method: "POST",
     headers: {
@@ -22,10 +27,9 @@ export function addItem(name, imageUrl, weather) {
   }).then(_checkResponse);
 }
 
-export function removeItem(id) {
+function removeItem(id) {
   const baseUrl =
     "https://my-json-server.typicode.com/Plouis53/se_project_react";
-
   return fetch(`${baseUrl}/items/${id}`, {
     method: "DELETE",
     headers: {
@@ -34,50 +38,10 @@ export function removeItem(id) {
   }).then(_checkResponse);
 }
 
-function _checkResponse(res) {
-  if (res.ok) {
-    return res.json();
-  }
-  return Promise.reject(`Error${res.status}`);
-}
+const itemsApi = {
+  get: getItems,
+  add: addItem,
+  remove: removeItem,
+};
 
-// function itemsApi() {
-//   function _checkResponse(res) {
-//     if (res.ok) {
-//       return res.json();
-//     }
-//     return Promise.reject(`Error${res.status}`);
-//   }
-
-//   const baseUrl =
-//     "https://my-json-server.typicode.com/Plouis53/se_project_react";
-
-//   return {
-//     get: () => {
-//       return fetch(`${baseUrl}/items`).then(_checkResponse);
-//     },
-//     add: (name, imageUrl, weather) => {
-//       return fetch(`${baseUrl}/items`, {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({
-//           name,
-//           imageUrl,
-//           weather,
-//         }),
-//       }).then(_checkResponse);
-//     },
-//     remove: (id) => {
-//       return fetch(`${baseUrl}/items/${id}`, {
-//         method: "DELETE",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//       }).then(_checkResponse);
-//     },
-//   };
-// }
-
-// export default itemsApi;
+export default itemsApi;
