476d12652f397b43cddf587090ed67f1b4f0df25
Plouis53 <Plouis53@gmail.com>
2023-05-16 23:27:33 -0400

    add on submit button

--- src/components/AddItemModal.js
+++ src/components/AddItemModal.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ModalWithForm from "./ModalWithForm";
+import "../components/App";
 
 const AddItemModal = ({ isOpen, onAddItem, handleCloseModal, buttonText }) => {
   const [name, setName] = useState("");
