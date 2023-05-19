ef72c0ff6280bb58ea1247ff6aa51ba459214dd9
Plouis53 <Plouis53@gmail.com>
2023-05-19 17:11:00 -0400

    _id with id

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,40 +2,39 @@ export const latitude = "33.7490";
 export const longitude = "-84.386330";
 export const APIkey = "09023fcd88dfb33405b9fe95d5351e01";
 
-
 export const defaultClothingItems = [
   {
-    _id: 0,
+    id: 0,
     name: "Cap",
     weather: "hot",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Cap.png?etag=f3dad389b22909cafa73cff9f9a3d591",
   },
   {
-    _id: 1,
+    id: 1,
     name: "Hoodie",
     weather: "warm",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Hoodie.png?etag=5f52451d0958ccb1016c78a45603a4e8",
   },
   {
-    _id: 2,
+    id: 2,
     name: "Jacket",
     weather: "cold",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Jacket.png?etag=f4bb188deaa25ac84ce2338be2d404ad",
   },
   {
-    _id: 3,
+    id: 3,
     name: "Sneakers",
     weather: "cold",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Sneakers.png?etag=3efeec41c1c78b8afe26859ca7fa7b6f",
   },
   {
-    _id: 4,
+    id: 4,
     name: "T-Shirt",
     weather: "hot",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/T-Shirt.png?etag=44ed1963c44ab19cd2f5011522c5fc09",
   },
   {
-    _id: 5,
+    id: 5,
     name: "Winter coat",
     weather: "cold",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Coat.png?etag=298717ed89d5e40b1954a1831ae0bdd4",
