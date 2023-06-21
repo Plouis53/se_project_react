7f1d8e6159faf1b98636b220c642250719efc4da
Plouis53 <Plouis53@gmail.com>
2023-06-21 15:35:30 -0400

    i ncluded weatheroptions, etc back

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,21 +1,25 @@
 import React, { useContext } from "react";
 import CurrentTemperatureUnitContext from "../contexts/CurrentTemperatureUnitContext";
+import { weatherOptions } from "../utils/constants";
 import { temperature } from "../utils/weatherApi";
 import "../blocks/WeatherCard.css";
 
 const WeatherCard = ({ day = true, type = "sunny", weatherTemp = 0 }) => {
   const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
 
+  const weatherOption = weatherOptions.find(
+    (option) => option.day === day && option.type === type
+  );
+  const imageSrcUrl = weatherOption?.url || "";
+
   const currentTempString = temperature(weatherTemp)[currentTemperatureUnit];
 
   return (
     <section className="weather">
       <div className="weather__temp-container">{currentTempString}</div>
-      <img
-        src={require(`../images/${type}.svg`)}
-        className="weather__image"
-        alt={type}
-      />
+      <div>
+        <img src={imageSrcUrl} className="weather__image" alt={type} />
+      </div>
       <section id="weather__info"></section>
     </section>
   );
