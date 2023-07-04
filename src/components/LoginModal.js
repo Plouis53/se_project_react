6281ac2fe919dc748c44fccd3b5e999d71f967db
Plouis53 <Plouis53@gmail.com>
2023-07-04 09:46:54 -0400

    updates x2

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -53,15 +53,6 @@ const LoginModal = ({
       handleSubmit={handleSubmit}
       buttonClass={buttonClasses}
       altButtonClick={handleRegisterClick}
-      // altButtonClick={handleSigninClick}
-      // title="Log in"
-      // name="Login"
-      // onClose={onClose}
-      // buttonText={buttonTexts}
-      // onOutClick={handleOutClick}
-      // handleSubmit={handleSubmit}
-      // buttonClass={buttonClasses}
-      // altButtonClick={handleRegisterClick}
     >
       <label className="modal__label">
         Email
@@ -96,81 +87,3 @@ const LoginModal = ({
 };
 
 export default LoginModal;
-
-// const [email, setEmail] = useState("");
-// const [password, setPassword] = useState("");
-
-// const handleLogin = () => {
-//   // Call the onLogin callback with the email and password
-//   onLogin(email, password);
-// };
-
-//   return (
-//     <div className="modal">
-//       <h2>Login</h2>
-//       <form>
-//         <div className="form-group">
-//           <label>Email</label>
-//           <input
-//             type="email"
-//             value={email}
-//             onChange={(e) => setEmail(e.target.value)}
-//           />
-//         </div>
-//         <div className="form-group">
-//           <label>Password</label>
-//           <input
-//             type="password"
-//             value={password}
-//             onChange={(e) => setPassword(e.target.value)}
-//           />
-//         </div>
-//         <button type="button" onClick={handleLogin}>
-//           Login
-//         </button>
-//       </form>
-//       <button type="button" onClick={onClose}>
-//         Close
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default LoginModal;
-
-// 61323 import React, { useState } from "react";
-
-// const LoginModal = ({ onSignIn }) => {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     onSignIn(email, password);
-//   };
-
-//   return (
-//     <div className="modal">
-//       <div className="modal__content">
-//         <h2>Login</h2>
-//         <form onSubmit={handleSubmit}>
-//           <input
-//             type="email"
-//             placeholder="Email"
-//             value={email}
-//             onChange={(e) => setEmail(e.target.value)}
-//           />
-//           <input
-//             type="password"
-//             placeholder="Password"
-//             value={password}
-//             onChange={(e) => setPassword(e.target.value)}
-//           />
-//           <button type="submit">Login</button>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default LoginModal;
